0843176acf7fc4107d3a80ac41e0bd31cad89fe4
Jake M <scudsterr@gmail.com>
2020-04-27 02:10:47 -0400

    Set Up Skeleton

---
+++ habit-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          SHOWER TIMER
+        </p>
+        <div>
+          <h1>Gonna Time here</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
