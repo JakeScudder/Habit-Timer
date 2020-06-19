ec226ac7220d6595bcc669f11e7e7029381ac89c
Jake Scudder <scudsterr@gmail.com>
2020-06-19 11:32:29 -0400

    Updates

---
+++ precache-manifest.326c6f61916d590d19b3509a7917128b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803c59a4883a48d84a65dc201e1eabd8",
+    "url": "/habit-timer/index.html"
+  },
+  {
+    "revision": "dfa0c41b3ee065bcb10e",
+    "url": "/habit-timer/static/css/main.c9d91f08.chunk.css"
+  },
+  {
+    "revision": "c92c8503b0f7c583f878",
+    "url": "/habit-timer/static/js/2.813c571f.chunk.js"
+  },
+  {
+    "revision": "99740f07e59da1777ca51e4cdac8fee9",
+    "url": "/habit-timer/static/js/2.813c571f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa0c41b3ee065bcb10e",
+    "url": "/habit-timer/static/js/main.97a73e27.chunk.js"
+  },
+  {
+    "revision": "3c60fd0dc80d1a49eff6",
+    "url": "/habit-timer/static/js/runtime-main.2f3d8652.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/habit-timer/static/media/logo.5d5d9eef.svg"
+  }
+]);
